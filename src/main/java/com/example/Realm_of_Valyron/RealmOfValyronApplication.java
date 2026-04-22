93d470b22ed3911ebb7dc2000d8a7ef00917ee8c
J4IM3LEZ <javier2072014@gmail.com>
2026-04-21 22:44:30 -0400

    Base de proyecto

---
+++ src/main/java/com/example/Realm_of_Valyron/RealmOfValyronApplication.java
@@ -0,0 +1,13 @@
+package com.example.Realm_of_Valyron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealmOfValyronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealmOfValyronApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Realm_of_Valyron/RealmOfValyronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Realm_of_Valyron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealmOfValyronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

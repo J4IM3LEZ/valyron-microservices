8b6deaf477b3118a72228632e0f789a5fc1f6810
J4IM3LEZ <javier2072014@gmail.com>
2026-04-26 02:39:26 -0400

    feat(etapa-1): eureka-server configurado y ms-auth completo con JWT, BCrypt y endpoints register/login

---
+++ ms-auth/src/main/java/com/realmofvalyron/ms_auth/dto/AuthResponse.java
@@ -0,0 +1,19 @@
+package com.realmofvalyron.msauth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String token;
+    private String username;
+    private String rol;
+    private String mensaje;
+
+}
